5ad1561524b1ea9313e27ab7a8f8703f253312de
Tyler Youschak <tyleryouschak@gmail.com>
2016-01-06 16:57:15 -0500

    Added check route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/check', function() {
+	return "works";
+});
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
